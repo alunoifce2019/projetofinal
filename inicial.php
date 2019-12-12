ad81914c4d01647ff653bf25125c9b1495e2af90
Anderson Araújo <andersonpaa.acop@gmail.com>
2019-12-12 10:55:28 -0300

    Inicial.php
    Modificação dos botões da tela inicial

--- inicial.php
+++ inicial.php
@@ -1,5 +1,5 @@
 <?php
-include "Home.php";
+echo "
   <!--==========================
   Header
   ============================-->
@@ -7,16 +7,16 @@ include "Home.php";
     <div class='container'>
       <div class='logo float-left'>
         <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <h1 class='text-light'><a href='#header'><span>NewBiz</span></a></h1> -->
+        <!-- <h1 class='text-light'><a href='#header'><span>GlFashion</span></a></h1> -->
         <a href='#intro' class='scrollto'><img src='img/logo.png' alt='' class='img-fluid'></a>
       </div>
       <nav class='main-nav float-right d-none d-lg-block'>
         <ul>
-          <li class='active'><a href='#intro'>Home</a></li>
-          <li><a href='#about'>About Us</a></li>
-          <li><a href='#services'>Services</a></li>
-          <li><a href='#portfolio'>Portfolio</a></li>
-          <li><a href='#team'>Team</a></li>
+          <li class='active'><a href='#intro'>Inicial</a></li>
+          <li><a href='#about'>Quem Somos</a></li>
+          <li><a href='#services'>Produtos</a></li>
+          <li><a href='#portfolio'>Time</a></li>
+          
           <li class='drop-down'><a href=''>Drop Down</a>
             <ul>
               <li><a href='#'>Drop Down 1</a></li>
@@ -34,7 +34,7 @@ include "Home.php";
               <li><a href='#'>Drop Down 5</a></li>
             </ul>
           </li>
-          <li><a href='#contact'>Contact Us</a></li>
+          <li><a href='#contact'>Contatos</a></li>
         </ul>
       </nav><!-- .main-nav -->
       
@@ -49,7 +49,7 @@ include "Home.php";
         <img src='img/intro-img.svg' alt='' class='img-fluid'>
       </div>
       <div class='intro-info'>
-        <h2>We provide<br><span>solutions</span><br>for your business!</h2>
+        <h2>Aqui você<br><span>é fashion</span></h2>
         <div>
           <a href='#about' class='btn-get-started scrollto'>Get Started</a>
           <a href='#services' class='btn-services scrollto'>Our Services</a>
@@ -57,4 +57,5 @@ include "Home.php";
       </div>
     </div>
   </section><!-- #intro -->
+  ";
   ?>
