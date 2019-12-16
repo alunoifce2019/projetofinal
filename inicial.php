5a61649b247a1be914be594ff5302fd9825b5f3a
Anderson Araújo <andersonpaa.acop@gmail.com>
2019-12-16 15:17:28 -0300

    Mudança no index, inicial, fornecedores e rodapé.
    Modificação

---
+++ inicial.php
@@ -0,0 +1,37 @@
+?php
+echo "
+
+ <header id="header" class="fixed-top">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
+        <a href="#intro" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul>
+          <li class='active'><a href='#intro'>Inicial</a></li>
+          <li><a href='#about'>Quem Somos</a></li>
+          
+          <li><a href='#portfolio'>Produtos</a></li>
+          <li><a href="#contact">Contato</a></li>
+          
+            
+            </ul>
+          </li>
+         
+      </nav><!-- .main-nav -->
+      
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro" class="clearfix">
+    
+  </section><!-- #intro -->
+";
+  ?>
---
+++ produtos.php
@@ -0,0 +1,143 @@
+?php
+echo "
+
+<section id='portfolio' class='clearfix'>
+      <div class='container'>
+        <header class='section-header'>
+          <h3 class='section-title'>Produtos</h3>
+        </header>
+        <div class='row'>
+          <div class='col-lg-12'>
+            <ul id='portfolio-flters'>
+              <li data-filter='*' class='filter-active'>Estoque</li>
+              <li data-filter='.filter-app'>Camisa</li>
+              <li data-filter='.filter-card'>Bermuda</li>
+              <li data-filter='.filter-web'>Calça</li>
+            </ul>
+          </div>
+        </div>
+        <div class='row portfolio-container'>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-app'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/app1.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Camisa Floral</a></h4>
+                <p>Camisa</p>
+                <div>
+                  <a href='img/portfolio/app1.jpg' data-lightbox='portfolio' data-title='App 1' class='link-preview' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-web' data-wow-delay='0.1s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/web3.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Calça Slim Sarja Vermelho</a></h4>
+                <p>Calça</p>
+                <div>
+                  <a href='img/portfolio/web3.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 3' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-app' data-wow-delay='0.2s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/app2.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Camisa Geometrico</a></h4>
+                <p>Camisa</p>
+                <div>
+                  <a href='img/portfolio/app2.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 2' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-card'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/card2.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Bermuda Tela Moletim Preta</a></h4>
+                <p>Bermuda</p>
+                <div>
+                  <a href='img/portfolio/card2.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 2' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-web' data-wow-delay='0.1s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/web2.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Calça Tela Preta</a></h4>
+                <p>Calça</p>
+                <div>
+                  <a href='img/portfolio/web2.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 2' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-app' data-wow-delay='0.2s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/app3.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Camisa Ilhos Destroyed</a></h4>
+                <p>Camisa</p>
+                <div>
+                  <a href='img/portfolio/app3.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 3' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-card'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/card1.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Bermuda Tela Moletim Branco</a></h4>
+                <p>Bermuda</p>
+                <div>
+                  <a href='img/portfolio/card1.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 1' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-card' data-wow-delay='0.1s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/card3.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Bermuda Slim Sarja Red</a></h4>
+                <p>Bermuda</p>
+                <div>
+                  <a href='img/portfolio/card3.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 3' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='col-lg-4 col-md-6 portfolio-item filter-web' data-wow-delay='0.2s'>
+            <div class='portfolio-wrap'>
+              <img src='img/portfolio/web1.jpg' class='img-fluid' alt=''>
+              <div class='portfolio-info'>
+                <h4><a href='#'>Calça Tela Preta</a></h4>
+                <p>Calça</p>
+                <div>
+                  <a href='img/portfolio/web1.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 1' title='Preview'><i class='ion ion-eye'></i></a>
+                  <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- #portfolio -->
+
+
+";
+  ?>
