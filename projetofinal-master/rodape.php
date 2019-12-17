db6666057c912ce7cd2119c51ccf7108447ad7ca
tonisvan <silvatonisvan@gmail.com>
2019-12-17 15:50:34 -0300

    modif
    modicamos

---
+++ projetofinal-master/rodape.php
@@ -0,0 +1,111 @@
+?php
+echo "
+<footer id='footer'>
+    <div class='footer-top'>
+      <div class='container'>
+        <div class='row'>
+
+          <div class='col-lg-4 col-md-6 footer-info'>
+            <h3>O que os clientes querem?</h3>
+            <p>O fato de os consumidores escolherem o seu negócio ao invés do da concorrência, é porque você oferece algo que eles desejam. Mais importante do que ser referência ou ter anos de atuação no mercado da moda é dar ao cliente o que ele quer encontrar.</p>
+          </div>
+
+          <div class='col-lg-2 col-md-6 footer-links'>
+            <h4>Atalhos</h4>
+            <ul>
+              <li><a href='#'>Inicial</a></li>
+              <li><a href='#'>Quem Somos</a></li>
+             
+             
+            </ul>
+          </div>
+
+          <div class='col-lg-3 col-md-6 footer-contact'>
+            <h4>Endereço / Contato</h4>
+            <p>
+             Vila Aroeira - Acopiara-CE <br>
+              <strong>Whatsapp:</strong> 9-99435821<br>
+              <strong>Instagram:</strong> glfashion10<br>
+            </p>
+
+            <div class='social-links'>
+              
+              <a href='#' class='facebook'><i class='fa fa-facebook'></i></a>
+              <a href='#' class='instagram'><i class='fa fa-instagram'></i></a>
+             <a href='#' class='facebook'> <i class='fa fa-whatsapp' aria-hidden='true'></i></a>
+
+            
+            </div>
+
+          </div>
+
+          <div class='col-lg-3 col-md-6 footer-newsletter'>
+            <h4>Desafiando Metas</h4>
+            <p><Br>'Para conquistar o sucesso você precisa acreditar nele a ponto de vêlo
+ acontecendo em sua vida'.</Br>
+
+<Br>'Você não pode mudar seu destino da noite para o dia, mas pode mudar sua direção'.</Br>
+
+<Br>'Todos querem ser lider, mas nem todos querem pagar o preço pela liderança'.</Br>
+
+<Br>'Não pare quando estiver cansado. Pare quando estiver tudo feito'.</Br>
+
+<Br>'Agora me responda, vai esperar acontecer?ou vai fazer acontecer?'</Br>
+
+<Br>'O periodo de maior ganho de conhecimento e experiência é o período 
+de maior dificuldade na vida de cada um'.<Br/>
+
+<Br>'Encontre um propósito pelo qual lutar e todas as vezes que
+ pensar em desistir lembre-se dele'.</Br>
+
+<Br>' Trate de fazer o possivel e deus cuidara do impossível.</Br></p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class='container'>
+      <div class='copyright'>
+        &copy; Copyright <strong>GL Fashion</strong>. All Rights Reserved
+      </div>
+      <div class='credits'>
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
+        -->
+        Designed by <a href='https://bootstrapmade.com/'>BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href='#' class='back-to-top'><i class='fa fa-chevron-up'></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id='preloader'></div> -->
+
+  <!-- JavaScript Libraries -->
+  <script src='lib/jquery/jquery.min.js'></script>
+  <script src='lib/jquery/jquery-migrate.min.js'></script>
+  <script src='lib/bootstrap/js/bootstrap.bundle.min.js'></script>
+  <script src='lib/easing/easing.min.js'></script>
+  <script src='lib/mobile-nav/mobile-nav.js'></script>
+  <script src='lib/wow/wow.min.js'></script>
+  <script src='lib/waypoints/waypoints.min.js'></script>
+  <script src='lib/counterup/counterup.min.js'></script>
+  <script src='lib/owlcarousel/owl.carousel.min.js'></script>
+  <script src='lib/isotope/isotope.pkgd.min.js'></script>
+  <script src='lib/lightbox/js/lightbox.min.js'></script>
+  <!-- Contact Form JavaScript File -->
+  <script src='contactform/contactform.js'></script>
+
+  <!-- Template Main Javascript File -->
+  <script src='js/main.js'></script>
+
+</body>
+</html>
+
+";
+  ?>
---
+++ projetofinal-master/sobrenos.php
@@ -0,0 +1,16 @@
+?php
+echo "
+
+<section id='about'>
+      <div class='container'>
+
+        <header class='section-header'>
+          <h3>Quem Somos</h3>
+          <p>A loja GL Fashion oferece o que há de mais moderno e atual em todas as categorias de produtos.
+Desde o princípio, nossa preocupação é oferecer os melhores produtos com os melhores preços 
+aliado a um ótimo atendimento a você cliente, que é a pessoa mais importante para nós.</p>
+        </header>
+          
+    </section><!-- #about -->
+";
+  ?>
